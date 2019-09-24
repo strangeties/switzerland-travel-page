e965dff4649ced9abd1ebfbeb69b627af107d3db
Wendy Nie <wendy90n@gmail.com>
2019-09-23 21:27:59 -0700

    Removes dependence on w3 libraries, which is a large file.

--- _main.js
+++ _main.js
@@ -10,6 +10,30 @@ kStories.forEach(function(item, index) {
                     is_hovering_over_button_[item] = false;
                  });
 
+function includeHtml(cb) {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                    elmnt.removeAttribute("w3-include-html");
+                    w3.includeHTML(cb);
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+    if (cb) cb();
+};
+
 function setMenuVisibilityToHidden(menu_style) {
     menu_style.visibility = 'hidden';
     menu_style.height = 0;
@@ -101,6 +125,8 @@ function resetMenuMaxHeight() {
 window.addEventListener("resize", function(){ resetMenuMaxHeight();});
 
 document.addEventListener("DOMContentLoaded", function(){
+                          includeHtml();
+                          
                           resetMenuMaxHeight();
                           
                           kStories.forEach(function(item, index) {
